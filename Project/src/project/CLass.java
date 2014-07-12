4657c20c0de12f0759125182ebf1ddb1f7b68b0b
Revoker7x <Revoker.kushma@gmail.com>
2014-07-12 13:22:26 -0500

    Rid of return

--- Project/src/project/CLass.java
+++ Project/src/project/CLass.java
@@ -11,16 +11,16 @@ public class CLass {
         
         mathing();
     }
-    public static double mathing() {
+    public static void mathing() {
         double num = Math.random()*100;
     
             if (num < 30){
                 System.out.println("THE NUMBER IS less than 30");
             }
             else{
-                System.out.println(num);
+                System.out.println("THE NUMBER IS more than 30");
             }
             System.out.println(num);
-        return num;
+       
     }
 }
