f81241f38cbe21a0a9c7708030d6a542d6c5d8b4
Revoker7x <Revoker.kushma@gmail.com>
2014-07-12 12:04:55 -0500

    fuck hello world

--- Project/src/project/CLass.java
+++ Project/src/project/CLass.java
@@ -5,8 +5,13 @@ public class CLass {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
+        System.out.println("Hello World, is the stupidest statement that "
+                 + "every programing example ever uses\nSTOP USING IT!!");
         
-    }
+        int number = 30;
+        if (number ==30){
+            System.out.println("THE NUMBER IS FUCKING 30");
+        }
+        }
     
 }
